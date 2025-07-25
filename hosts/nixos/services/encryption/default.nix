97503aeabaec8c48bddbb5694c0f968f6a9ee6b7
dbochoa77 <dbochoa77@outlook.com>
2025-07-25 16:27:00 -0700

    updated

--- hosts/nixos/services/encryption/default.nix
+++ hosts/nixos/services/encryption/default.nix
@@ -2,7 +2,7 @@
 
 {
   
-  boot.initrd.luks.devices."luks-6f5a683b-a714-46fb-88b5-db4d59342ef4".device = "/dev/disk/by-uuid/6f5a683b-a714-46fb-88b5-db4d59342ef4";
+  boot.initrd.luks.devices."luks".device = "/dev/disk/by-uuid/UUID";
 
 }
 
