2ca85e75a2e73eff28c7afa88de54024ac71fbe3
dbochoa77 <dbochoa77@outlook.com>
2025-07-27 11:29:55 -0700

    working on remote X11 ssh

--- hosts/nixos/services/ssh/default.nix
+++ hosts/nixos/services/ssh/default.nix
@@ -6,4 +6,14 @@
      settings.PermitRootLogin = "no";
      allowSFTP = true;
    };
+
+    services.openssh.extraConfig = ''
+    X11Forwarding yes
+    X11DisplayOffset 10
+    X11UseLocalhost yes
+  '';
+  
+  environment.systemPackages = with pkgs; [
+    xorg.xauth
+  ];
 }
