7162f61d993a61d6759f6c50eaeea127dee0e72e
dbochoa77 <dbochoa77@outlook.com>
2025-07-27 12:43:46 -0700

    containers work

--- hosts/nixos/services/containers/echo/default.nix
+++ hosts/nixos/services/containers/echo/default.nix
@@ -4,6 +4,7 @@
     extraOptions = ["-text='Hello, World!'" "--network=web"];
     ports = ["5678:5678"];
   };
+
   system.activationScripts.createPodmanNetworkWeb = lib.mkAfter ''
     if ! /run/current-system/sw/bin/podman network exists web; then
       /run/current-system/sw/bin/podman network create web
