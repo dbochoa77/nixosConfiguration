c28d50a50d90638827bb07ef8be86b071739a758
dbochoa77 <dbochoa77@outlook.com>
2025-07-27 12:54:22 -0700

    prepping to rebuild on host

--- hosts/nixos/secrets.nix
+++ hosts/nixos/secrets.nix
@@ -5,6 +5,9 @@
     secrets = {
       secret1 = {
          file = "${inputs.self}/secrets/secret1.age";
+         owner = "dbochoa77";
+         group = "dbochoa77";
+         path = "home/dbochoa77/.secret1";
         };
       };
     };
