9dfb350a4965c8ccdd6d33d28e2788ef7f59cbfa
dbochoa77 <dbochoa77@proton.me>
2025-07-27 21:45:48 -0700

    added nerd fonts

--- hosts/nixos/services/fonts/default.nix
+++ hosts/nixos/services/fonts/default.nix
@@ -1,9 +1,19 @@
 { pkgs, config, ... }:
 
 {
-    fonts.fonts = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.ubuntu
-    nerd-fonts.hack
+    fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        proggyfonts
+  ];
+
+    fonts.packages = with pkgs; [ 
+        nerdfonts 
   ];
 }
