358eb8b07a7d34f2767f86a30f3c36e237eaadb5
dbochoa77 <dbochoa77@proton.me>
2025-08-25 10:23:35 -0700

    updated virtualization getting windows to work, issues with tpm

--- hosts/nixos/services/virtualization/default.nix
+++ hosts/nixos/services/virtualization/default.nix
@@ -1,12 +1,26 @@
 { config, pkgs, ... }:
- 
-{ 
+
+{
   programs.virt-manager.enable = true;
-   
-  users.groups.libvirtd.members = ["dbochoa77"];
-   
-  virtualisation.libvirtd.enable = true;
-   
-  virtualisation.spiceUSBRedirection.enable = true;
+
+  users.users.dbochoa77.extraGroups = [ "libvirtd" "kvm" ];
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        # (optional) make sure we’re on the KVM build
+        package = pkgs.qemu_kvm;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];  # has VARS/SecureBoot
+        swtpm.enable = true;                   # expose TPM to libvirt
+      };
+    };
+
+    spiceUSBRedirection.enable = true;
+    swtpm.enable = true;                       # run the software TPM
+  };
+
+  security.tpm2.enable = true;                 # host TPM (optional, safe)
 }
 
