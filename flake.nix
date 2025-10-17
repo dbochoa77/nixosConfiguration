579adc02b13e71be5672d99c93a22f725a057924
dbochoa77 <dbochoa77@proton.me>
2025-10-16 19:02:30 -0700

    reverted changes

--- flake.nix
+++ flake.nix
@@ -6,7 +6,8 @@
         inputs.nixpkgs.follows = "nixpkgs";
       };
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+      nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
       
       nvimDotfiles = {
